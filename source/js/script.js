50c0da119a5299476aec6ccc680f903d96c2a35e
Crow26 <7tacson7@gmail.com>
2018-10-25 19:40:21 +0300

    Добавлена интерактивная карта при рабочем JavaScript

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,8 @@
 var menuBtn = document.querySelector(".header-page__menu");
 var mobileMenu = document.querySelector(".navigation__list");
+var wrapperMap = document.querySelector(".map__wrapper");
+var imageMap = document.querySelector(".map__image");
+var interactiveMap = document.querySelector(".map__interactive-map");
 
 if(menuBtn.classList.contains("no-js-btn-nav")) {
   menuBtn.classList.remove("no-js-btn-nav");
@@ -7,6 +10,18 @@ if(menuBtn.classList.contains("no-js-btn-nav")) {
 
 if(mobileMenu.classList.contains("no-js-nav")) {
   mobileMenu.classList.remove("no-js-nav");
+}
+
+if(wrapperMap.classList.contains("no-js-wrapper-map")) {
+  wrapperMap.classList.remove("no-js-wrapper-map");
+}
+
+if(imageMap.classList.contains("no-js-image-map")) {
+  imageMap.classList.remove("no-js-image-map");
+}
+
+if(interactiveMap.classList.contains("no-js-map")) {
+  interactiveMap.classList.remove("no-js-map");
 }
 
 menuBtn.addEventListener("click", function(e) {
