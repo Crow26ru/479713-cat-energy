4cb507fbe538867ef1d4b9449e7bc501f0757390
Crow26 <7tacson7@gmail.com>
2018-11-04 14:41:23 +0300

    Убраны лишние пробелы и добавлен обработчик для тач события

--- source/js/input.js
+++ source/js/input.js
@@ -18,7 +18,7 @@ formPush.addEventListener("click", function(e) {
   var weight = form.querySelector(".form__input--weight")
   var email = form.querySelector(".form__input--email");
   var phone = form.querySelector(".form__input--phone");
-  
+
   if(name.value == "") {
     e.preventDefault();
 
@@ -45,7 +45,7 @@ formPush.addEventListener("click", function(e) {
 
   if(!validEmail(email.value)) {
     e.preventDefault();
-    
+
     if(!email.classList.contains("form__input--text-error")) {
       email.classList.add("form__input--text-error");
     }
@@ -57,7 +57,63 @@ formPush.addEventListener("click", function(e) {
 
   if(!validPhone(phone.value)) {
     e.preventDefault();
-    
+
+    if(!phone.classList.contains("form__input--text-error")) {
+      phone.classList.add("form__input--text-error");
+    }
+  } else {
+    if(phone.classList.contains("form__input--text-error")) {
+      phone.classList.remove("form__input--text-error");
+    }
+  }
+});
+
+formPush.addEventListener("touchend", function(e) {
+  var form = document.querySelector(".form");
+  var name = form.querySelector(".form__input--name");
+  var weight = form.querySelector(".form__input--weight")
+  var email = form.querySelector(".form__input--email");
+  var phone = form.querySelector(".form__input--phone");
+
+  if(name.value == "") {
+    e.preventDefault();
+
+    if(!name.classList.contains("form__input--text-error")) {
+      name.classList.add("form__input--text-error");
+    }
+  } else {
+    if(name.classList.contains("form__input--text-error")) {
+      name.classList.remove("form__input--text-error");
+    }
+  }
+
+  if(weight.value == "" || isNaN(weight.value)) {
+    e.preventDefault();
+
+    if(!weight.classList.contains("form__input--text-error")) {
+      weight.classList.add("form__input--text-error");
+    }
+  } else {
+    if(weight.classList.contains("form__input--text-error")) {
+      weight.classList.remove("form__input--text-error");
+    }
+  }
+
+  if(!validEmail(email.value)) {
+    e.preventDefault();
+
+    if(!email.classList.contains("form__input--text-error")) {
+      email.classList.add("form__input--text-error");
+    }
+  } else {
+    if(email.classList.contains("form__input--text-error")) {
+      email.classList.remove("form__input--text-error");
+    }
+  }
+
+  if(!validPhone(phone.value)) {
+    e.preventDefault();
+
     if(!phone.classList.contains("form__input--text-error")) {
       phone.classList.add("form__input--text-error");
     }
